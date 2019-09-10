30b294641d58ae45b28a178e511533d8f291e355
github username <github email address>
2019-09-10 14:42:11 -0500

    updated routing for all routes

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ import NewUserForm from "./login/NewUserForm";
 import { Route, Switch } from "react-router-dom";
 
 const queryURL = "http://localhost:3000/search";
-const fetchURL = "http://localhost:3000" // Host
-const fetchUsers = fetchURL + "/users"
-const fetchTeams = fetchURL + "/teams"
+const fetchURL = "http://localhost:3000"; // Host
+const fetchUsers = fetchURL + "/users";
+const fetchTeams = fetchURL + "/teams";
 const fetchUserTeams = fetchURL + "/user_teams";
 
 class App extends Component {
@@ -25,50 +25,53 @@ class App extends Component {
     showTeamContainer: true
   };
 
-componentDidMount() {
+  componentDidMount() {
     this.fetchUsers();
     this.fetchTeams();
   }
 
   fetchYelp = (location, term) => {
-    const querySearch = queryURL
+    const querySearch = queryURL;
     fetch(querySearch, {
       method: "POST",
-      headers: {"content-type": "application/json", "accept": "application/json"},
+      headers: {
+        "content-type": "application/json",
+        accept: "application/json"
+      },
       body: JSON.stringify({
         term: term,
         location: location
       })
     })
-    .then(res => res.json())
-    .then(data => this.setState({yelpResults: data.results}))
-    .catch(err => console.log(err))
-  }
-  
+      .then(res => res.json())
+      .then(data => this.setState({ yelpResults: data.results }))
+      .catch(err => console.log(err));
+  };
+
   fetchUsers = () => {
     fetch(fetchUsers)
       .then(res => res.json())
-      .then(users => this.setState({ allUsers: users }))
-  }
+      .then(users => this.setState({ allUsers: users }));
+  };
 
   fetchTeams = () => {
     fetch(fetchTeams)
       .then(res => res.json())
-      .then(teams => this.setState({ allTeams: teams }))
-  }
+      .then(teams => this.setState({ allTeams: teams }));
+  };
 
   getCurrentTeam = newTeamObj => {
-    this.setState({ currentTeam: newTeamObj })
-  }
+    this.setState({ currentTeam: newTeamObj });
+  };
 
   selectContainer = e => {
-    this.setState({showTeamContainer: !this.state.showTeamContainer})
-  }
+    this.setState({ showTeamContainer: !this.state.showTeamContainer });
+  };
 
-// set state of current user ID after the login is successful
-  getUserId = (userid) => {
-    this.setState({currentUserId: userid})
-  }
+  // set state of current user ID after the login is successful
+  getUserId = userid => {
+    this.setState({ currentUserId: userid });
+  };
 
   render() {
     const {
@@ -78,47 +81,69 @@ componentDidMount() {
       currentTeam,
       showTeamContainer,
       yelpResults
-    } = this.state
-    const currentUser = allUsers.find(user => user.id === currentUserId)
-    console.log(this.state)
+    } = this.state;
+    const currentUser = allUsers.find(user => user.id === currentUserId);
+    console.log(this.state);
 
-    return (
+    return allUsers.length > 0 ? (
       <div className="login-container">
-        {/* <Link to="/login">Login</Link> */}
 
-        {/* <LoginForm getUserId={this.getUserId} /> */}
-        <Route 
-        path="/login" 
-        render={() => (<LoginForm getUserId={this.getUserId} />)} />
-      </div>
-    )
-    
+        <Route
+          path="/login"
+          render={() => <LoginForm getUserId={this.getUserId} />}
+        />
+        <Route
+          path="/newteam"
+          render={() => (
+            <TeamContainer
+              allUsers={allUsers}
+              allTeams={allTeams}
+              currentUser={currentUser}
+              fetchURL={fetchURL}
+              getCurrentTeam={this.getCurrentTeam}
+              currentTeam={currentTeam}
+              selectContainer={this.selectContainer}
+            />
+          )}
+        />
+        <Route
+          path="/roulette"
+          render={() => (
+            <RouletteContainer
+              fetchYelp={this.fetchYelp}
+              yelpResults={yelpResults}
+              selectContainer={this.selectContainer}
+            />
+          )}
+        />
+      </div> 
+    ) : "Loading..." 
+
     // return allUsers.length > 0 ? (
-    //   <div>
-    //     {/* <ProfileContainer currentUser={currentUser}/> */}
-    //     {showTeamContainer ? (
-    //       <TeamContainer
-    //         allUsers={allUsers}
-    //         allTeams={allTeams}
-    //         currentUser={currentUser}
-    //         fetchURL={fetchURL}
-    //         getCurrentTeam={this.getCurrentTeam}
-    //         currentTeam={currentTeam}
-    //         selectContainer={this.selectContainer}
-    //       />
-    //     ) : (
-    //       <RouletteContainer 
-    //         fetchYelp={this.fetchYelp} 
-    //         yelpResults={yelpResults}
-    //         selectContainer={this.selectContainer}
-    //       />
+    // <div>
+    //   {/* <ProfileContainer currentUser={currentUser}/> */}
+    //   {showTeamContainer ? (
+    //     <TeamContainer
+    //       allUsers={allUsers}
+    //       allTeams={allTeams}
+    //       currentUser={currentUser}
+    //       fetchURL={fetchURL}
+    //       getCurrentTeam={this.getCurrentTeam}
+    //       currentTeam={currentTeam}
+    //       selectContainer={this.selectContainer}
+    //     />
+    // ) : (
+    // <RouletteContainer
+    //   fetchYelp={this.fetchYelp}
+    //   yelpResults={yelpResults}
+    //   selectContainer={this.selectContainer}
+    // />
     //     )}
     //   </div>
     // ) : (
     //   <div>Loading...</div>
     // )
-    
   }
 }
 
-export default App
+export default App;
